2467259a49de1e29a50c8257e2c235c6fb4e2961
govindongit <govindnag5863@gmail.com>
2025-01-13 18:04:15 +0530

    Nutrition and Calorie Tracker - React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NutritionMeter from './component/NutritionMeter/NutritionMeter';
+
+function App() {
+  return (
+  <div className="bg-gray-100 min-h-screen">
+    <NutritionMeter/>
+  </div>
+  );
+}
+
+export default App;
